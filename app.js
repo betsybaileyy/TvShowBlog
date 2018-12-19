a208eccacfad40f75690e40b075ddf2dd259f58d
betsybaileyy <betsy.bailey80@gmail.com>
2018-12-19 14:38:52 -0500

    able to see single post

--- app.js
+++ app.js
@@ -31,10 +31,19 @@ app.get('/posts/new', (req, res) => {
 });
 
 
+app.get('/posts/:id', (req, res) => {
+    Post.findById(req.params.id).then((post) => {
+        res.render('posts-show', { post: post })
+    }).catch((err) => {
+        console.log(err.message)
+    })
+});
+
+
 app.post('/posts', (req, res) => {
     Post.create(req.body).then((post) => {
         console.log(post);
-        res.redirect('/');
+        res.redirect(`/posts/${post._id}`);
     }).catch((err) => {
         console.log(err.message);
     });
