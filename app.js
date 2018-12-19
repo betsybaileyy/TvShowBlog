0c0074bc808c57ff2ed131705f9a09f7988e4216
betsybaileyy <betsy.bailey80@gmail.com>
2018-12-19 13:59:58 -0500

    mongoDB up and (hopefully) running)

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
 var exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes-take-2');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let posts = [
-    { title: "My Favorite Show, Currently", showTitle: "The Americans" },
-    { title: "Must Watch Show", showTitle: "Ray Donovan" }
-];
+const Post = mongoose.model('Post', {
+    title: String,
+    showTitle: String
+});
+
+// let posts = [
+//     { title: "My Favorite Show, Currently", showTitle: "The Americans" },
+//     { title: "Must Watch Show", showTitle: "Ray Donovan" }
+// ];
+
+app.get('/', (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.render('posts-index', { posts: posts });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
 
 app.get('/', (req, res) => {
     res.render('posts-index', { posts: posts });
