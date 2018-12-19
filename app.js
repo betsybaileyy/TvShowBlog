ed6b0c478fe26b9a34ce9d9354247ba12ddeabfc
betsybaileyy <betsy.bailey80@gmail.com>
2018-12-19 12:14:07 -0500

    users can see all posts

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+let posts = [
+    { title: "My Favorite Show, Currently", showTitle: "The Americans" },
+    { title: "Must Watch Show", showTitle: "Ray Donovan" }
+];
+
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are cool' });
+    res.render('posts-index', { posts: posts });
 });
 
 app.listen(3000, () => {
